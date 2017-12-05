21582c5826f7e5b2e1cc5341a725ec291ac8b6d2
sy@bizsaas.cn <zxcvbnm>
2017-12-05 18:41:34 +0800

    增加AES对称加密工具和测试用例

---
+++ src/test/java/com/biz/util/AESTest.java
@@ -0,0 +1,44 @@
+package com.biz.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by suyi on 2017/12/5.
+ */
+public class AESTest {
+    @Test
+    public void aesEncrypt() throws Exception {
+        String content = "我爱你";
+        System.out.println("加密前：" + content);
+
+        String key = "123456";
+        System.out.println("加密密钥和解密密钥：" + key);
+
+        String encrypt = AES.aesEncrypt(content, key);
+        System.out.println("加密后：" + encrypt);
+
+        assertEquals("stLEywX13EVhsA3jT6E20Q==",encrypt);
+
+    }
+
+    @Test
+    public void aesDecrypt() throws Exception {
+
+
+                String content = "stLEywX13EVhsA3jT6E20Q==";
+        System.out.println("加密前：" + content);
+
+        String key = "123456";
+        System.out.println("加密密钥和解密密钥：" + key);
+
+
+
+        String decrypt = AES.aesDecrypt(content, key);
+        System.out.println("解密后：" + decrypt);
+
+        assertEquals("我爱你",decrypt);
+    }
+
+}
